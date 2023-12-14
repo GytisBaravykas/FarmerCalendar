867b881d7418105a475393dd654ff84368327b49
GytisBaravykas <gytis.baravykas@gmail.com>
2023-12-14 02:22:25 +0200

    Separate wiki and calendar.

---
+++ app/src/main/java/com/example/farmercalendar/WikiFirst.kt
@@ -0,0 +1,23 @@
+package com.example.farmercalendar
+
+import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import com.example.farmercalendar.databinding.ActivityMainBinding
+import com.example.farmercalendar.databinding.WikiDesignBinding
+import java.util.Objects
+
+class WikiFirst : AppCompatActivity() {
+    private lateinit var binding: WikiDesignBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = WikiDesignBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar.myToolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.toolbar.myToolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+    }
+}
